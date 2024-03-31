9e9586e32791cc6e1945776ca9d5faac0c67240f
Felipe V. Rigo <github@felipevr.eti.br>
2024-03-31 19:09:15 -0400

    Segunda versão da lista de compras

--- exercicios/lista01/01-lista_de_compras.py
+++ exercicios/lista01/01-lista_de_compras.py
@@ -1,19 +1,22 @@
 # ## Lista de compras
 
-# ### Primeira versão da lista de compras
+# ### Segunda versão da lista de compras
+# 
 
-# Escreva um programa que permita que um usuário crie uma lista de compras.
-# O usuário deve ser capaz de adicionar itens, remover itens e visualizar a lista.
+# Mude o programa de lista de compras para usar um dicionário ao invés de uma lista.
+# O programa deve ter as mesmas funcionalidades, mas agora deve ser possível
+# adicionar mais de uma unidade de um item na lista de compras. Ou seja, o dicionário
+# deve armazenar o nome do item e a quantidade desejada pelo usuário. Por exemplo,
+# se o usuário digitar "banana" e "2", o dicionário deve armazenar "banana" como chave
+# e 2 como valor. A estrutura do dicionário ficaria assim: `{"banana": 2}`.
 # 
-# Estruture seu programa da seguinte forma:
+# O programa deve permitir que o usuário adicione, remova e visualize o dicionário de compras.
 # 
-# 1. Crie uma lista vazia para armazenar os itens da lista de compras.
-# 2. Crie um loop infinito que imprima um menu de opções ao usuário e permita que ele escolha uma opção.
-# 3. Dentro do loop, use uma declaração if para executar a tarefa apropriada de acordo com a escolha do usuário.
-# 4. Se o usuário escolher adicionar um item, solicite que ele digite o nome do item e adicione-o à lista.
-# 5. Se o usuário escolher remover um item, solicite que ele digite o nome do item e remova-o da lista.
-# 6. Se o usuário escolher ver a lista, mostre cada item da lista em sua própria linha.
-# 7. Se o usuário escolher sair, encerre o loop usando break.
+# Além disso, o programa deve mostrar uma mensagem de erro se o usuário tentar
+# usar uma opção inválida do menu. Por exemplo, se o usuário digitar 5, o programa
+# deve mostrar a mensagem "Opção inválida. Por favor, escolha uma opção válida." e
+# mostrar o menu novamente. Além disso, o programa deve ser *case insensitive*, ou seja,
+# "Maçã" e "maçã" devem ser considerados o mesmo item.
 # 
 # Exemplo de saída:
 # 
@@ -24,6 +27,7 @@
 # 4 Sair
 # Escolha uma opção: 1
 # Digite um item: banana
+# Digite a quantidade: 2
 # 
 # 1 Adicionar item
 # 2 Remover item
@@ -31,13 +35,14 @@
 # 4 Sair
 # Escolha uma opção: 1
 # Digite um item: maçã
+# Digite a quantidade: 3
 # 
 # 1 Adicionar item
 # 2 Remover item
 # 3 Ver lista
 # 4 Sair
 # Escolha uma opção: 3
-# ['banana', 'maçã']
+# {'banana': 2, 'maçã': 3}
 # 
 # 1 Adicionar item
 # 2 Remover item
@@ -45,13 +50,14 @@
 # 4 Sair
 # Escolha uma opção: 2
 # Digite um item: banana
+# Digite a quantidade: 1
 # 
 # 1 Adicionar item
 # 2 Remover item
 # 3 Ver lista
 # 4 Sair
 # Escolha uma opção: 3
-# ['maçã']
+# {'banana': 1, 'maçã': 3}
 # 
 # 1 Adicionar item
 # 2 Remover item
@@ -59,9 +65,10 @@
 # 4 Sair
 # Escolha uma opção: 4
 # ```
+# 
 
 
-lista_compras = []
+lista_compras = {}
 
 def imprime_menu():
     print("1 Adicionar item")
@@ -69,6 +76,21 @@ def imprime_menu():
     print("3 Ver lista")
     print("4 Sair")
     return input("Escolha uma opção: ")
+
+def inclui_novo_item():
+    item = input("Digite um item: ")
+    while(True):
+        qtd = input("Digite a quantidade: ")
+        if qtd.isdecimal():
+            break
+        else:
+            print("Quantidade inválida. Tente novamente.")
+    item = item.casefold()
+    qtd = int(qtd)
+    if item in lista_compras:
+        lista_compras[item] += qtd
+    else:
+        lista_compras[item] = qtd
 
 continua = True
 
@@ -81,13 +103,13 @@ while(continua):
     elif escolha == '3':
         print(lista_compras)
     elif escolha == '1':
-        item = input("Digite um item: ")
-        lista_compras.append(item)
+        inclui_novo_item()
     elif escolha == '2':
         item = input("Digite um item: ")
+        item = item.casefold()
         if item in lista_compras:
-            lista_compras.remove(item)
+            lista_compras.pop(item)
         else:
             print('Item não encontrado!')
     else:
-        print('Opção inválida')
+        print('Opção inválida. Por favor, escolha uma opção válida.')
