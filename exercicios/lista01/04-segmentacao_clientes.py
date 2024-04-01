6abdee0c9b1af242a0eccdf7696d6d50fb881267
Felipe V. Rigo <github@felipevr.eti.br>
2024-03-31 20:56:50 -0400

    Segunda versão da segmentação de clientes

--- exercicios/lista01/04-segmentacao_clientes.py
+++ exercicios/lista01/04-segmentacao_clientes.py
@@ -1,6 +1,6 @@
 ### Segmentação de clientes
 
-### Primeira versão da segmentação de clientes
+### Segunda versão da segmentação de clientes
 
 """
 Escreva um programa que segmenta clientes com base em suas compras totais.
@@ -30,9 +30,33 @@ João: Segmento do Cliente = Bronze
 Maria: Segmento do Cliente = Prata
 José: Segmento do Cliente = Ouro
 ```
+
+Valida a entrada do usuário.
+Se o usuário digitar um valor inválido para compras, mostre a mensagem
+"Entrada inválida. Por favor, digite um número para compras." e peça para o usuário digitar
+novamente. Tal validação deve ser feita usando try/except.
+
+Além disso, ao invés de deixar os limites de compras fixos no programa, armazene-os em uma
+lista de tuplas. Por exemplo:
+
+```python
+segmentos = [(1000, 'Bronze'), (5000, 'Prata'), (float('inf'), 'Ouro')]
+``` 
+
+Assim, outros segmentos podem ser adicionados facilmente. O primeiro elemento da tupla é o
+limite de compras e o segundo é o nome do segmento. O último elemento da lista é uma tupla
+com limite `float('inf')`, que representa o segmento Ouro. Isso significa que, se o valor de
+compras for maior que todos os limites, o segmento será Ouro.
+
+Depois, percorra essa lista e, para cada tupla, verifique se o valor de compras é menor ou igual
+ao limite. Se for, armazene o segmento em um dicionário. Por exemplo, se o usuário digitar
+"João" e "500" para compras, o dicionário deve ficar assim:
+`{'João': 'Bronze'}`
+
 """
 
 clientes = {}
+segmentos = [(1000, 'Bronze'), (5000, 'Prata'), (float('inf'), 'Ouro')]
 
 def isfloat(num):
     try:
@@ -42,7 +66,10 @@ def isfloat(num):
         return False
     
 def solicitar_nome_cliente() -> str:
-    item = input("Digite o nome do cliente (ou 'fim' para finalizar): ")
+    item = ''
+    while(len(item) < 1):
+        item = input("Digite o nome do cliente (ou 'fim' para finalizar): ")
+        
     return item.casefold()
 
 def solicitar_compras() -> float:
@@ -56,19 +83,19 @@ def solicitar_compras() -> float:
     return float(compra)
     
 def atualizar_dados(cliente:str, compra:float):
-    clientes[cliente] = compra
+    if cliente in clientes:
+        print('Cliente já cadastrado! Nenhum valor atualizado')
+        return
+    
+    for limite,segmento in segmentos:
+        if compra <= limite:
+            clientes[cliente] = segmento
+            break
 
-def pega_segmento(valor:float) -> str:
-    if valor <= 1000:
-        return 'Bronze'
-    if valor <= 5000:
-        return 'Prata'
-    # > 5000
-    return 'Ouro'
 
 def print_dados():
     for item in clientes:
-        segmento = pega_segmento(clientes[item])
+        segmento = clientes[item]
         print(f"{item}: Segmento do Cliente = {segmento}")
 
 
