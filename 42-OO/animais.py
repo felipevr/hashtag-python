4e48b755f92668b5f6463815013746c7498e76a4
Felipe V. Rigo <github@felipevr.eti.br>
2024-04-29 21:43:15 -0400

    testes de heranças multiplas

--- 42-OO/animais.py
+++ 42-OO/animais.py
@@ -1,16 +1,44 @@
 
 class Aquatico:
+    def __init__(self):
+        super().__init__()
+        print("1 nada")
+
     def nadar(self):
         pass
 
 class Terrestre:
+    def __init__(self):
+        super().__init__()
+        print("2 anda")
+
     def andar(self):
         pass
 
 class Anfibio(Terrestre, Aquatico):
+
+    def __init__(self):
+        super().__init__()
+        print("3 nada e anda")
+
     pass
 
+class Mamifero():
+    def mamar(self):
+        pass
+    def amamentar(self):
+        pass
 
+class Aves():
+    def __init__(self):
+        super().__init__()
+        print("4 voa")
+        
+    def voar(self):
+        pass
+
+class Cachorro(Mamifero, Terrestre):
+    pass
 
 peixe = Aquatico()
 
@@ -18,4 +46,6 @@ gato = Terrestre()
 
 sapo = Anfibio()
 
-print((peixe, gato, sapo))
+auau = Cachorro()
+
+print((peixe, gato, sapo, auau))
